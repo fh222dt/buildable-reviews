92cf52c1d1c46031e63445043f2a94c8a1c64a2b
fh222dt <fh222dt@student.lnu.se>
2016-04-01 10:40:27 +0200

    starting review plugin with biolerplate

---
+++ includes/class-buildable-reviews-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Buildable_reviews
+ * @subpackage Buildable_reviews/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Buildable_reviews
+ * @subpackage Buildable_reviews/includes
+ * @author     Your Name <email@example.com>
+ */
+class Buildable_reviews_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
