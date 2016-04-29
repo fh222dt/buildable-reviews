dd96d4ddd6708c49e28d7dbad5c9488d371282fe
fh222dt <fh222dt@student.lnu.se>
2016-04-29 11:16:04 +0200

    render question based on type

---
+++ public/class-question-templates.php
@@ -0,0 +1,59 @@
+<?php
+
+public function render_question($question, $type){
+                                                    //TODO: lägg till classer
+    $output = '<div class="br-question>"
+                <h3>'. $question['name'] .'</h3>
+                <p>'. $question['desc'] .'</p>';
+
+    $output .= '<div class="br-answer-options">';
+
+    $options = $question['options'];
+    if($type = 'checkbox') {
+
+        $output .= render_checkbox($options);
+    }
+    else if($type = 'textfield') {
+
+        $output .= render_textfield($options);
+    }
+
+    else if($type = 'scale' || $type = 'radio') {            //TODO: kanske fel namn??
+
+        $output .= render_radio($options, $question);
+    }
+
+    $output .=         '</div>';
+
+    $output .= '</div>';
+
+    return $output;
+}
+
+public function render_checkbox($options) {
+
+    foreach ($options as $option) {        //TODO: name???
+        $output .= '<input type="checkbox" name="'. $option['name'] .'" value="'. $option['name'] .'"></input>
+                    <label>'. $option['name'] .'</label>';
+    }
+    //TODO: gruppera ??
+
+    return $output;
+}
+
+public function render_textfield($options) {
+
+    //foreach ($options as $option) {        //TODO: name???
+        $output .= '<textarea name="'. $option['name'] .'"></textarea>';
+    //}
+    return $output;
+}
+
+public function render_radio($options, $question) {    //kanske ett 3e arg för hur det ska stylas??
+
+    foreach ($options as $option) {        //TODO: name???
+        $output .= '<input type="radio" name="'. $question['name'] .'" value="'. $option['name'] .'"></input>
+                    <label>'. $option['name'] .'</label>';
+    }
+    return $output;
+}
