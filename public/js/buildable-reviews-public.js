1ed49281340a352765ca36c1bad7bd16fa1e6449
fh222dt <fh222dt@student.lnu.se>
2016-05-30 19:09:34 +0200

    click event for '3 buttons'

--- public/js/buildable-reviews-public.js
+++ public/js/buildable-reviews-public.js
@@ -1,32 +1,29 @@
 (function( $ ) {
 	'use strict';
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+	$( window ).load(function() {
+
+		$('#br-review-button').click(function(e) {
+			e.preventDefault();
+			$('#review-form').toggle('slow', function() {
+
+			});
+		});
+
+		$('#br-view-all-button').click(function(e) {
+			e.preventDefault();
+			$('#list-all-reviews').toggle('slow', function() {
+
+			});
+		});
+
+		$('#br-summary-button').click(function(e) {
+			e.preventDefault();
+			$('#review-summary').toggle('slow', function() {
+
+			});
+		});
+
+	});
 
 })( jQuery );
